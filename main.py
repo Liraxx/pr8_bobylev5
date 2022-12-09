aa09ac732197010f94915170be63869db4beaf9d
Lirax <temuk_huk@mail.ru>
2022-12-09 21:24:49 +0300

    Модернизировали файл main.py на ветке dev

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-password = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+password = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890')
 random.shuffle(password)
 password = ''.join([random.choice(password) for x in range(11)])
 print (password)
